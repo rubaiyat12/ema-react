d3aa0e8cf4df8de6a0f87586afba8cfe73076d69
Rubaiyat Razin <rubaiyatraha12@gmail.com>
2023-03-27 23:31:50 +0600

    cart-part

---
+++ src/components/Product/Product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+const Product = (props) => {
+    const { name, img, price, seller, ratings } = props.product;
+    const { handleAddtoCart } = props;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p>Price:${price}</p>
+            <p><small>Seller:{seller}</small></p>
+                <p><small>Ratings:{ratings}</small></p>
+            </div>
+            <button onClick={()=> handleAddtoCart(props.product)} className='btn-cart'>
+                <p>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
