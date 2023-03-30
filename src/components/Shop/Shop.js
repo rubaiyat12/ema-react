783d30c806f4a809ec5620d56f7cfca3263e6895
Rubaiyat Razin <rubaiyatraha12@gmail.com>
2023-03-30 22:00:32 +0600

    final commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -11,10 +12,27 @@ const Shop = () => {
             .then(res => res.json())
            .then(data=>setProducts(data))
     }, [])
+
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const saveCart=[];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => products.id === id)
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);
+
+            }
+            
+        }
+        setCart(saveCart);
+    },[products])
     const handleAddtoCart = (product) => {
         console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
         
     }
     return (
